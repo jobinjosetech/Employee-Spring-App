2d2b685f2bc08a6661b0478da1a3486742901ed1
jobin <jobinjose.in@gmail.com>
2022-12-06 14:43:18 +0530

    fixes

--- src/main/java/com/nest/EmployeeApp/controller/EmployeeController.java
+++ src/main/java/com/nest/EmployeeApp/controller/EmployeeController.java
@@ -1,6 +1,9 @@
 package com.nest.EmployeeApp.controller;
 
+import com.nest.EmployeeApp.model.Employee;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,22 +14,30 @@ public class EmployeeController {
         return "Welcome to Employee App";
     }
 
-    @GetMapping("/add")
-    public String AddEmployee(){
+    @PostMapping(path="/add", consumes = "application/json", produces = "application/json")
+    public String AddEmployee(@RequestBody Employee e){
+        System.out.println(e.getEmpcode());
+        System.out.println(e.getEmpname());
+        System.out.println(e.getDesignation());
+        System.out.println(e.getSalary());
+        System.out.println(e.getCompanyname());
+        System.out.println(e.getMobile());
+        System.out.println(e.getUsername());
+        System.out.println(e.getPassword());
         return "Welcome to Employee Add Page";
     }
 
-    @GetMapping("/search")
+    @PostMapping("/search")
     public String SearchEmployee(){
         return "Welcome to Employee Search Page";
     }
 
-    @GetMapping("/delete")
+    @PostMapping("/delete")
     public String DeleteEmployee(){
         return "Welcome to Employee Delete Page";
     }
 
-    @GetMapping("/edit")
+    @PostMapping("/edit")
     public String EditEmployee(){
         return "Welcome to Employee Edit Page";
     }
